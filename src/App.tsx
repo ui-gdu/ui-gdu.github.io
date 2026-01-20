d12f601dfec603b54549628cdc42da6f54072cd0
Michael Crapse <mikeeybikeey@gmail.com>
2026-01-20 00:25:54 -0800

    Prefix paths with gdu

--- src/App.tsx
+++ src/App.tsx
@@ -67,42 +67,42 @@ function shuffle<T>(array: T[]): T[] {
 
 const pastGames = shuffle([
   // jam 1
-  { title: 'Fresh Out the Oven', image: '/games/fresh_out_the_oven.png', link: 'https://mdseven2020.itch.io/fresh-out-the-oven' },
-  { title: 'Baugette Blaster', image: '/games/baugette_blaster.png', link: 'https://theskyedoctor.itch.io/baugette-blaster' },
-  { title: 'The Tallest Sandwich', image: '/games/the_tallest_sandwich.png', link: 'https://socksnsandles.itch.io/the-tallest-sandwich' },
-  { title: 'You\'re Toast!', image: '/games/default.png', link: 'https://dullahan914.itch.io/youre-toast' },
-  { title: 'Rye-se of Nations DoughLuxe Edition (v2)', image: '/games/rye_se.png', link: 'https://coastal-wolf.itch.io/rye-se-of-nations-doughlux-edition' },
-  { title: 'Bread Battle', image: '/games/bread_battle.png', link: 'https://drbryce27.itch.io/bread-battle' },
-  { title: 'I Want a Sandwich', image: '/games/i_want_a_sandwich.png', link: 'https://mikeeybikeey.itch.io/i-want-a-sandwich' },
-  { title: 'Sandwich Attack', image: '/games/default.png', link: 'https://thetcgguy.itch.io/sandwich-attack' },
-  { title: 'Stuffed', image: '/games/stuffed.png', link: 'https://proph-et.itch.io/stuffed' },
-  { title: 'Doh!', image: '/games/doh.png', link: 'https://valcore737.itch.io/doh' },
-  { title: 'Clash of Bread', image: '/games/clash_of_bread.png', link: 'https://glopmeister.itch.io/clash-of-bread' },
+  { title: 'Fresh Out the Oven', image: '/gdu/games/fresh_out_the_oven.png', link: 'https://mdseven2020.itch.io/fresh-out-the-oven' },
+  { title: 'Baugette Blaster', image: '/gdu/games/baugette_blaster.png', link: 'https://theskyedoctor.itch.io/baugette-blaster' },
+  { title: 'The Tallest Sandwich', image: '/gdu/games/the_tallest_sandwich.png', link: 'https://socksnsandles.itch.io/the-tallest-sandwich' },
+  { title: 'You\'re Toast!', image: '/gdu/games/default.png', link: 'https://dullahan914.itch.io/youre-toast' },
+  { title: 'Rye-se of Nations DoughLuxe Edition (v2)', image: '/gdu/games/rye_se.png', link: 'https://coastal-wolf.itch.io/rye-se-of-nations-doughlux-edition' },
+  { title: 'Bread Battle', image: '/gdu/games/bread_battle.png', link: 'https://drbryce27.itch.io/bread-battle' },
+  { title: 'I Want a Sandwich', image: '/gdu/games/i_want_a_sandwich.png', link: 'https://mikeeybikeey.itch.io/i-want-a-sandwich' },
+  { title: 'Sandwich Attack', image: '/gdu/games/default.png', link: 'https://thetcgguy.itch.io/sandwich-attack' },
+  { title: 'Stuffed', image: '/gdu/games/stuffed.png', link: 'https://proph-et.itch.io/stuffed' },
+  { title: 'Doh!', image: '/gdu/games/doh.png', link: 'https://valcore737.itch.io/doh' },
+  { title: 'Clash of Bread', image: '/gdu/games/clash_of_bread.png', link: 'https://glopmeister.itch.io/clash-of-bread' },
 
   // jam 2
-  { title: 'Upside Doom', image: '/games/upsidedoom.png', link: 'https://chudbert.itch.io/upsidedoom' },
-  { title: 'Shift', image: '/games/shift.png', link: 'https://socksnsandles.itch.io/shift' },
-  { title: 'Paper Cuts', image: '/games/paper_cuts.png', link: 'https://coastal-wolf.itch.io/paper-cuts' },
-  { title: 'Glob Game', image: '/games/glob_game.png', link: 'https://ky246.itch.io/globs-united' },
-  { title: 'The Depths Above', image: '/games/the_depths_above.png', link: 'https://proph-et.itch.io/the-depths-above' },
-  { title: 'Sir Sheep Ferdinand', image: '/games/default.png', link: 'https://mikeeybikeey.itch.io/sir-sheep-ferdinand' },
+  { title: 'Upside Doom', image: '/gdu/games/upsidedoom.png', link: 'https://chudbert.itch.io/upsidedoom' },
+  { title: 'Shift', image: '/gdu/games/shift.png', link: 'https://socksnsandles.itch.io/shift' },
+  { title: 'Paper Cuts', image: '/gdu/games/paper_cuts.png', link: 'https://coastal-wolf.itch.io/paper-cuts' },
+  { title: 'Glob Game', image: '/gdu/games/glob_game.png', link: 'https://ky246.itch.io/globs-united' },
+  { title: 'The Depths Above', image: '/gdu/games/the_depths_above.png', link: 'https://proph-et.itch.io/the-depths-above' },
+  { title: 'Sir Sheep Ferdinand', image: '/gdu/games/default.png', link: 'https://mikeeybikeey.itch.io/sir-sheep-ferdinand' },
 
   // jam 3
-  { title: 'Wren', image: '/games/wren.png', link: 'https://jumbo78.itch.io/wren' },
-  { title: 'SpaceBar', image: '/games/spacebar.png', link: 'https://socksnsandles.itch.io/spacebar' },
-  { title: 'Void Avoider', image: '/games/void_avoider.png', link: 'https://ajx21.itch.io/void-avoider' },
-  { title: 'Titled Goose Game', image: '/games/titled_goose_game.png', link: 'https://drbryce27.itch.io/titled-goose-game' },
-  { title: 'This is (Not) Asteroids', image: '/games/this_is_not_asteroids.png', link: 'https://truetrashmage.itch.io/this-is-not-asteroids' },
-  { title: 'Re-Delivery', image: '/games/re_delivery.png', link: 'https://tateorrtot.itch.io/re-delivery' },
+  { title: 'Wren', image: '/gdu/games/wren.png', link: 'https://jumbo78.itch.io/wren' },
+  { title: 'SpaceBar', image: '/gdu/games/spacebar.png', link: 'https://socksnsandles.itch.io/spacebar' },
+  { title: 'Void Avoider', image: '/gdu/games/void_avoider.png', link: 'https://ajx21.itch.io/void-avoider' },
+  { title: 'Titled Goose Game', image: '/gdu/games/titled_goose_game.png', link: 'https://drbryce27.itch.io/titled-goose-game' },
+  { title: 'This is (Not) Asteroids', image: '/gdu/games/this_is_not_asteroids.png', link: 'https://truetrashmage.itch.io/this-is-not-asteroids' },
+  { title: 'Re-Delivery', image: '/gdu/games/re_delivery.png', link: 'https://tateorrtot.itch.io/re-delivery' },
 
   // jam 4
-  { title: 'Ducks in a Row', image: '/games/ducks_in_a_row.png', link: 'https://etbay.itch.io/ducks-in-a-row' },
-  { title: 'Purrfect Pet', image: '/games/purrfect_pet.png', link: 'https://coastal-wolf.itch.io/purrfect-pet' },
-  { title: 'Doorway To Life', image: '/games/doorway_to_life.png', link: 'https://tateorrtot.itch.io/doorway-to-life' },
-  { title: 'Photo-Frenzy', image: '/games/photo_frenzy.png', link: 'https://socksnsandles.itch.io/photo-frenzy' },
-  { title: 'Last Light', image: '/games/last_light.png', link: 'https://valleyrock06.itch.io/last-light' },
-  { title: 'Light My Way', image: '/games/light_my_way.png', link: 'https://extreme-kitten.itch.io/light-my-way' },
-  { title: 'Game Jam 1', image: '/games/default.png', link: 'https://vandallllll.itch.io/game-jam-1' },
+  { title: 'Ducks in a Row', image: '/gdu/games/ducks_in_a_row.png', link: 'https://etbay.itch.io/ducks-in-a-row' },
+  { title: 'Purrfect Pet', image: '/gdu/games/purrfect_pet.png', link: 'https://coastal-wolf.itch.io/purrfect-pet' },
+  { title: 'Doorway To Life', image: '/gdu/games/doorway_to_life.png', link: 'https://tateorrtot.itch.io/doorway-to-life' },
+  { title: 'Photo-Frenzy', image: '/gdu/games/photo_frenzy.png', link: 'https://socksnsandles.itch.io/photo-frenzy' },
+  { title: 'Last Light', image: '/gdu/games/last_light.png', link: 'https://valleyrock06.itch.io/last-light' },
+  { title: 'Light My Way', image: '/gdu/games/light_my_way.png', link: 'https://extreme-kitten.itch.io/light-my-way' },
+  { title: 'Game Jam 1', image: '/gdu/games/default.png', link: 'https://vandallllll.itch.io/game-jam-1' },
 ]);
 
 const backgroundImages = pastGames.map(v => v.image).filter(v => !v.endsWith('default.png'));
@@ -358,14 +358,14 @@ const TestimonialsSection = () => {
 }
 
 const mediaImages = [
-  '/events/fo2025_jam3.jpg',
-  '/events/fc2025_jam3.jpg',
-  '/events/fo2025_jam4.jpg',
-  '/events/jam.jpg',
-  '/events/s2025.jpg',
-  '/events/so2025.jpg',
-  '/events/team_at_breakfast_jam4.jpg',
-  '/events/team_breakfast.webp',
+  '/gdu/events/fo2025_jam3.jpg',
+  '/gdu/events/fc2025_jam3.jpg',
+  '/gdu/events/fo2025_jam4.jpg',
+  '/gdu/events/jam.jpg',
+  '/gdu/events/s2025.jpg',
+  '/gdu/events/so2025.jpg',
+  '/gdu/events/team_at_breakfast_jam4.jpg',
+  '/gdu/events/team_breakfast.webp',
 ];
 
 const MediaSection: React.FC = () => (
@@ -428,7 +428,7 @@ const SponsorsSection: React.FC = () => (
       <Box sx={{ textAlign: 'center' }}>
         <Box
           component="img"
-          src="/sponsors/lightcast.jpg"
+          src='/gdu/sponsors/lightcast.jpg'
           alt="Lightcast"
           sx={{ height: 256, mb: 2 }}
         />
