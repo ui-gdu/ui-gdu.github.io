4a68eded4ca8ce5ff946e64c0d3c515ab2644ec1
Michael Crapse <mikeeybikeey@gmail.com>
2026-06-28 20:33:15 -0600

    Update for jam 7

--- src/JamDetails.tsx
+++ src/JamDetails.tsx
@@ -1,19 +1,19 @@
 export default {
     // The name of the game jam
-    name: "Game Jam #6",
+    name: "Game Jam #7",
 
     // The location for the opening and closing ceremony
     location: "Janssen Engineering Building (JEB) 126 - Think Tank",
 
     // Opening ceremony
-    openingDate: "April 17 (Friday)",
-    openingTime: "5:00 PM",
+    openingDate: "TBA", // Example: "April 17 (Friday)",
+    openingTime: "TBA", // Example: "5:00 PM",
 
     // Lan Party
-    lanPartyDate: "April 20 (Monday)",
-    lanPartyTime: "5:15 PM",
+    lanPartyDate: "TBA", // Example: "April 20 (Monday)",
+    lanPartyTime: "TBA", // Example "5:15 PM",
 
     // Closing ceremony
-    closingDate: "April 24 (Friday)",
-    closingTime: "5:00 PM",
+    closingDate: "TBA", // Example: "April 24 (Friday)",
+    closingTime: "TBA", // Example: "5:00 PM",
 }
